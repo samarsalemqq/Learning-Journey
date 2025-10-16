529a98e07fac5d71091f703b70f2870a0c16655c
Samar <samarsalemqq@gmail.com>
2025-10-16 15:06:58 +0300

    Initial Commit

---
+++ Learning Journey/Learning_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyApp.swift
+//  Learning Journey
+//
+//  Created by Samar A on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learning JourneyTests/Learning_JourneyTests.swift
@@ -0,0 +1,16 @@
+//
+//  Learning_JourneyTests.swift
+//  Learning JourneyTests
+//
+//  Created by Samar A on 24/04/1447 AH.
+//
+
+import Testing
+
+struct Learning_JourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
